607e273618f67637d27af2d0062d2e51dcc036b4
sandeep.dawre@cloudethix.com <sandeep.dawre@cloudethix.com>
2023-09-04 16:45:54 +0530

    changes added

--- 15_TERRAFORM_DYNAMIC/main.tf
+++ 15_TERRAFORM_DYNAMIC/main.tf
@@ -30,6 +30,23 @@ resource "aws_security_group" "this_sg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
